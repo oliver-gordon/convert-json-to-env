f2879b39764a59164a133d8de478762401b49814
Oliver <oliver.cs@pm.me>
2021-01-10 15:29:05 +0000

    json file conversion control flow

---
+++ cmd/cmd.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"converter/util"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	file       string
+	validity   bool
+	fileFormat string
+)
+
+func Run() {
+	printFlag := flag.Bool("print", false, "Print the conversion instead of outputting to a file.")
+	flag.Parse()
+	fileArgs := flag.Args()
+
+	if len(fileArgs) == 1 {
+		file = fileArgs[0]
+		validity, fileFormat = util.CheckFileFormat(file)
+	} else if len(fileArgs) > 1 || len(fileArgs) < 1 {
+		fmt.Println("invalid value for file")
+		os.Exit(0)
+	}
+
+	if validity != true {
+		fmt.Printf("Invalid file format for file: %s. Need .json", fileFormat)
+		os.Exit(0)
+	}
+
+	if fileFormat == "json" {
+		ConvertJSON(file, *printFlag)
+	} else {
+		fmt.Println(`¯\_(ツ)_/¯`)
+		os.Exit(0)
+	}
+}
---
+++ cmd/convertJSON.go
@@ -0,0 +1,75 @@
+package cmd
+
+import (
+	"converter/util"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	openedFile map[string]interface{}
+	envVars    []string
+)
+
+func ConvertJSON(fileName string, printOnly bool) {
+	file, err := ioutil.ReadFile(fileName)
+	json.Unmarshal(file, &openedFile)
+	if err != nil {
+		fmt.Printf("Error reading file: %s", fileName)
+		os.Exit(0)
+	}
+	parseMap(openedFile, "")
+
+	if printOnly {
+		printMap(envVars)
+	} else {
+		WriteToFile(envVars)
+	}
+}
+
+func parseMap(jsonData map[string]interface{}, parentKey string) {
+	for key, val := range jsonData {
+		switch concreteVal := val.(type) {
+		case map[string]interface{}:
+			parseMap(val.(map[string]interface{}), key)
+		default:
+			var newEnv string
+			if parentKey == "" {
+				newEnv = fmt.Sprintf("%s=%v \n", strings.ToUpper(key), concreteVal)
+			} else {
+				newEnv = fmt.Sprintf("%s.%s=%v \n", strings.ToUpper(parentKey), strings.ToUpper(key), concreteVal)
+			}
+			envVars = append(envVars, newEnv)
+		}
+
+	}
+}
+
+func printMap(envVars []string) {
+	for i := 0; i < len(envVars); i++ {
+		fmt.Printf("%s", envVars[i])
+	}
+}
+
+func WriteToFile(dataToWrite []string) {
+	envFileName := fmt.Sprintf("%s.env", time.Now().Format("15-04-05"))
+	file, err := os.Create(envFileName)
+	if err != nil {
+		fmt.Println("unable to create new file")
+		os.Exit(0)
+	}
+
+	defer util.CloseFile(file)
+
+	for _, item := range dataToWrite {
+		_, err := file.WriteString(item)
+		if err != nil {
+			fmt.Println("unable to write to file")
+			os.Exit(0)
+		}
+	}
+}
